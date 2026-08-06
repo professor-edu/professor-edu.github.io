42bc9655aad256888d2a42123670bf943af33eb3
professor-edu <jeduardosoeiro@gmail.com>
2026-08-06 23:41:57 +0100

    PRE_4

---
+++ pre/catalogo-pre.js
@@ -0,0 +1,56 @@
+// ======================================================================
+// CATÁLOGO CENTRAL DE JOGOS DA PRÉ
+// Sempre que criar um jogo novo, é necessário adicioná-lo a esta lista!
+// O site vai atualizar as contagens e os sorteios automaticamente.
+// ======================================================================
+
+const todosOsJogos =[
+
+    { nome: "Já Sei Contar"},
+    { nome: "Super Cores"},
+    { nome: "Diferenças"},
+    { nome: "Memória Mágica"},
+    { nome: "Sombras" },
+    { nome: "Detetive"},
+    { nome: "Super Detetive"},
+    { nome: "Sequências"},
+    { nome: "Frente e Trás" }, 
+    { nome: "Rimas" }, 
+    { nome: "Reciclagem" },
+    { nome: "Super Memória" },
+	{ nome: "Contar Sílabas" }, 
+	{ nome: "Vogais" }, 
+	{ nome: "Aprendo a Contar" }, 
+	{ nome: "Vogais Faladas" }, 
+	{ nome: "Labirintos" }, 
+	{ nome: "Sem Pares" }, 
+	{ nome: "Puzzles Mágicos" }, 
+	{ nome: "Números Coloridos" },
+	{ nome: "Dados Mágicos"},
+	{ nome: "Sons dos Bichos"},
+	{ nome: "Quem Sou Eu?"},
+	{ nome: "O Robô Construtor?"},
+	{ nome: "Pixel Art"},
+	{ nome: "Vamos Colorir"},
+	{ nome: "Memória Falada"},
+	{ nome: "Metades"},
+	{ nome: "Som Inicial"},
+	{ nome: "Ouvir e Descobrir"},
+	{ nome: "Ouvir e Resolver"},
+	{ nome: "A Magia dos Números"},
+	{ nome: "Magia Musical"},
+	{ nome: "Os Cestos do Leo"},
+	{ nome: "O Monstro das Formas"},
+	{ nome: "A Cidade dos sentidos"},
+	{ nome: "O Monstro das Emoções"},
+	{ nome: "A Roleta Maluca"},
+	{ nome: "O Animal Favorito"},
+	{ nome: "A Casa dos Animais"},
+	{ nome: "Os Nossos Heróis"},
+	{ nome: "Detetives do Planeta"},
+	{ nome: "O Ratinho Comilão"},
+	{ nome: "O Polvo Pintor"},
+	{ nome: "Esquerda ou Direita?"},
+	{ nome: "Nomes e Tamanhos"},
+	{ nome: "O Radar"}
+];
